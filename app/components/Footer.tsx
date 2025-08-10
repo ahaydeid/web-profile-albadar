f4505d7b7256e371e6a69c6257e49c526109ed94
ahaydeid <adi.hadi270@gmail.com>
2025-08-10 20:54:00 +0700

    Tambah footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="bg-purple-700 border-t-4 text-white text-center py-4">
+      <p className="font-semibold">© 2025 Al Badar Tangerang. All rights reserved</p>
+      <p className="text-sm text-gray-200">made by ahadi</p>
+    </footer>
+  );
+}
