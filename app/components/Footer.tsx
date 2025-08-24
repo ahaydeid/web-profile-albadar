f173bcedbeeac8ed2e6751b474360fd539762b7f
ahaydeid <adi.hadi270@gmail.com>
2025-08-25 03:14:44 +0700

    Tambah link footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,11 @@ export default function Footer() {
   return (
     <footer className="bg-purple-700 border-t-4 text-white text-center py-4">
       <p className="font-semibold">© 2025 Al Badar Tangerang. All rights reserved</p>
-      <p className="text-sm text-gray-200">made by ahadi</p>
+      <p className="text-sm text-gray-200">
+        <a href="https://www.instagram.com/ahayde_hadd/" target="_blank" rel="noopener noreferrer" className="hover:underline hover:text-gray-300 transition-colors">
+          made by ahadi
+        </a>
+      </p>
     </footer>
   );
 }
