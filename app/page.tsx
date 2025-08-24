0efc34b503023f4b99527dc4418243923aa42bd2
ahaydeid <adi.hadi270@gmail.com>
2025-08-25 02:50:29 +0700

    update link sosmed

--- app/page.tsx
+++ app/page.tsx
@@ -38,18 +38,19 @@ export default function HomePage() {
 
             {/* Social */}
             <div className="flex gap-4 mt-15">
-              <a href="https://wa.me/628123456789" target="_blank" rel="noopener noreferrer">
-                <Image src="/assets/icons/wa.png" alt="WhatsApp" width={40} height={40} className="hover:opacity-80 transition" />
+              <a href="https://wa.me/6285814888500" target="_blank" rel="noopener noreferrer" aria-label="Hubungi via WhatsApp" className="transition transform hover:scale-110 hover:shadow-lg rounded-full">
+                <Image src="/assets/icons/wa.png" alt="WhatsApp" width={40} height={40} />
               </a>
 
-              <a href="mailto:email@example.com">
-                <Image src="/assets/icons/mail.png" alt="Email" width={40} height={40} className="hover:opacity-80 transition" />
+              <a href="mailto:smksalbadaroke@gmail.com" aria-label="Kirim Email" className="transition transform hover:scale-110 hover:shadow-lg rounded-full">
+                <Image src="/assets/icons/mail.png" alt="Email" width={40} height={40} />
               </a>
 
-              <a href="https://instagram.com/username" target="_blank" rel="noopener noreferrer">
-                <Image src="/assets/icons/instagram.png" alt="Instagram" width={40} height={40} className="hover:opacity-80 transition" />
+              <a href="https://www.instagram.com/smk_albadar/?hl=id" target="_blank" rel="noopener noreferrer" aria-label="Kunjungi Instagram" className="transition transform hover:scale-110 hover:shadow-lg rounded-full">
+                <Image src="/assets/icons/instagram.png" alt="Instagram" width={40} height={40} />
               </a>
             </div>
+
             <div className="flex gap-4 mt-4">
               <button className="bg-blue-600 text-white font-bold px-6 py-3 md:px-8 md:py-3 text-sm sm:text-lg rounded-full">Kontak Kami</button>
               <button className="bg-yellow-400 text-black font-bold px-6 py-3 md:px-8 md:py-3 text-sm sm:text-lg">Daftar Sekarang</button>
